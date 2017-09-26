858c36719f49d0e611d1cd407acfe31fe11ed81e
Wndrr <mathieu@viales.fr>
2017-09-27 00:26:01 +0200

    Add run command

---
+++ Mjml/Mjml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace Wndrr.Mjml
+{
+    public static class Mjml
+    {
+        private static string _nodePath;
+        private static string _mjmlPath;
+        private static string _mjmlContent;
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        public static void Init(string mjmlContent, string mjmlPath = null, string nodePath = null)
+        {
+            _mjmlContent = mjmlContent;
+            _mjmlPath = mjmlPath ?? Path.Combine(AssemblyDirectory, ".bin", "mjmlFromString");
+            _nodePath = nodePath ?? Path.Combine(AssemblyDirectory, ".bin", "Node.exe");
+        }
+
+        public static string Render()
+        {
+            var output = string.Empty;
+            
+            var command = $"$htmlOutput = {_nodePath} {_mjmlPath} -c \"{_mjmlContent}\"";
+
+            var powerShell = PowerShell.Create();
+
+            powerShell.AddScript(command);
+            powerShell.Invoke();
+
+            if (powerShell.Runspace.SessionStateProxy.GetVariable("htmlOutput") is object[] t)
+            {
+                output = t.Cast<PSObject>().Aggregate(output, (current, item) => current + item.BaseObject.ToString());
+            }
+
+            if (powerShell.Streams.Error.Count > 0)
+            {
+                output = powerShell.Streams.Error.Aggregate(output, (current, err) => current + (err + "<br />"));
+            }
+            return output;
+
+        }
+    }
+}
--- Mjml/Properties/AssemblyInfo.cs
+++ Mjml/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mjml")]
+[assembly: AssemblyTitle("Wndrr.Mjml")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mjml")]
+[assembly: AssemblyProduct("Wndrr.Mjml")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
