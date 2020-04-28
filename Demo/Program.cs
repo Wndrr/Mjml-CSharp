abc25bc9b54c84f8491e872547dfa99fab79e46e
taylus <brandon@legacy.codes>
2020-04-28 10:24:32 -0500

    Show demo email in the browser

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Diagnostics;
 using System.IO;
 using Wndrr.Mjml.CSharp;
 
@@ -13,10 +13,9 @@ namespace Demo
             
             var mjmlTemplate = "<mjml><mj-body><mj-section><mj-column><mj-image width=\"100px\" src=\"/assets/img/logo-small.png\"></mj-image><mj-divider border-color=\"#F45E43\"></mj-divider><mj-text font-size=\"20px\" color=\"#F45E43\" font-family=\"helvetica\">Hello World</mj-text></mj-column></mj-section></mj-body></mjml>";
             var outputHtml = Mjml.Render(mjmlTemplate);
-            
-            Console.Write(outputHtml);
 
-            Console.ReadKey();
+            File.WriteAllText("output.html", outputHtml);
+            Process.Start(new ProcessStartInfo() { FileName = "output.html", UseShellExecute = true });
         }
     }
 }
