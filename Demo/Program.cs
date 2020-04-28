68dd6cbb0e73b627972a936e2703cd793e61d662
taylus <brandon@legacy.codes>
2020-04-28 10:25:46 -0500

    Read demo template from file

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -10,8 +10,8 @@ namespace Demo
         {
             Mjml.PathRepository.NodePath = @"C:\Program Files\nodejs\node.exe";
             Mjml.PathRepository.TmpPath = Path.GetTempPath();
-            
-            var mjmlTemplate = "<mjml><mj-body><mj-section><mj-column><mj-image width=\"100px\" src=\"/assets/img/logo-small.png\"></mj-image><mj-divider border-color=\"#F45E43\"></mj-divider><mj-text font-size=\"20px\" color=\"#F45E43\" font-family=\"helvetica\">Hello World</mj-text></mj-column></mj-section></mj-body></mjml>";
+
+            var mjmlTemplate = File.ReadAllText("example.mjml");
             var outputHtml = Mjml.Render(mjmlTemplate);
 
             File.WriteAllText("output.html", outputHtml);
